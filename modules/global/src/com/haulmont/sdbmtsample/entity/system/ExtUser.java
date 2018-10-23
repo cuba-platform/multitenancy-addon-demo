57948b1acdda7c7ca4b2167d2937d68d6f5d0b67
Evgeny Zaharchenko <zaharchenko@haulmont.com>
2018-10-23 09:48:35 +0400

    fix issue with tenant editor

--- modules/global/src/com/haulmont/sdbmtsample/entity/system/ExtUser.java
+++ modules/global/src/com/haulmont/sdbmtsample/entity/system/ExtUser.java
@@ -6,12 +6,14 @@ import javax.persistence.Entity;
 
 import com.haulmont.addon.sdbmt.core.TenantId;
 import com.haulmont.addon.sdbmt.entity.HasTenant;
+import com.haulmont.chile.core.annotations.NamePattern;
 import com.haulmont.cuba.core.entity.annotation.Extends;
 import com.haulmont.cuba.security.entity.User;
 
 @Extends(User.class)
 @DiscriminatorValue("EXT")
 @Entity(name = "sdbmtsample$ExtUser")
+@NamePattern("#getCaption|login,name,tenantId")
 public class ExtUser extends User implements HasTenant {
     private static final long serialVersionUID = -2598354095282194816L;
 
